f7a5f6624bdaca5056cf23bfcb0fd98eb57aff69
Harshad.N <harshad.n4321@gmail.com>
2022-12-23 12:27:44 +0530

    readme

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,13 @@ import './About.css'
 
 export default function About() {
     return (
-        <div style={{ color: 'white', textAlign: 'center' }}>About</div>
+        <div style={{ color: 'white', textAlign: 'center' }}>About
+            <div style={{ color: 'white', textAlign: 'center' }}>
+                <p>
+                    this todoApp Build by <a href="https://github.com/harshad4321" target="_blank" style={{ color: 'red' }}>Harshad.N</a>
+                </p>
+            </div>
+        </div >
     )
+
 }
